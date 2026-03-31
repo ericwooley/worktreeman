5e70a425fdf6902557436bfbc56e6f8055465cc1
Eric Wooley <ericwooley@gmail.com>
2026-03-31 17:18:13 +0000

    Fix command for dev prod

--- dev-prod.sh
+++ dev-prod.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 WORKDIR=$1
+npm pack
+VERSION=$(cat package.json | jq '.version' -r)
+echo "Using version $VERSION"
 if [ -z $WORKDIR ]; then
   WORKDIR=".."
   echo "Defaulting to .. for work dir"
 else
-  ehco "Starting With Dir $WORKDIR"
+  echo "Starting With Dir $WORKDIR"
 fi
-npm pack
-VERSION=$(cat package.json | jq '.version' -r)
-echo "Using version $VERSION"
-npx -y worktreeman-$VERSION.tgz start --cwd .. --host=auto
+npx -y worktreeman-$VERSION.tgz start --cwd $WORKDIR --host=auto
